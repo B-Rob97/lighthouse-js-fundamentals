116f66ea0540015c61b2ba87d450a398e4a8edcd
Brett Roberts <brett.roberts@live.ca>
2023-05-12 09:03:28 -0600

    Added diiference for console.log vs return

--- hello.js
+++ hello.js
@@ -6,3 +6,20 @@ sayHello('Brett');
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+// console.log vs return
+
+// console.log
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+// return
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
