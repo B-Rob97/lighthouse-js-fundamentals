de362f335c28ed2ac41e509f5bd9dd46b5ff0a9f
Brett Roberts <brett.roberts@live.ca>
2023-05-16 00:36:14 -0600

    Voting station function

--- votingStation.js
+++ votingStation.js
@@ -1,14 +1,18 @@
 const chooseStations = function(stations) {
   var goodStations = [];
+
+  // Iterate through each station in the stations array
   for (const station of stations) {
+    // Check if the capacity is greater than or equal to 20 and the type is either "school" or "community centre"
     if (station[1] >= 20 && (station[2] === "school" || station[2] === "community centre")) {
+      // If the conditions are met, add the station name (at index 0) to the goodStations array
       goodStations.push(station[0]);
-    };
-  }; 
+    }
+  }
+
+  // Return the array of good stations
   return goodStations;
 };
-
-
 
 const stations = [
   ['Big Bear Donair', 10, 'restaurant'],
@@ -16,4 +20,4 @@ const stations = [
   ['Moose Mountain Community Centre', 45, 'community centre']
 ];
 
-console.log(chooseStations(stations));
+console.log(chooseStations(stations)); // Output: ['Bright Lights Elementary', 'Moose Mountain Community Centre']
