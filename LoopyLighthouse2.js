3b32b5573bb6b0e8c04a13816d32b26265b98da3
Brett Roberts <brett.roberts@live.ca>
2023-08-24 19:03:01 -0600

    Major Refactor

--- LoopyLighthouse2.js
+++ LoopyLighthouse2.js
@@ -1,36 +1,19 @@
-function loopyLighthouse(range, multiples, words) {
-  // Initialize the loop variable 'x' to the first value in the 'range' array
+const loopyLighthouse = function(range, multiples, words) {
   let x = range[0];
+
+  const isMultipleOfFirst = (x, multiples) => x % multiples[0] === 0;
+  const isMultipleOfSecond = (x, multiples) => x % multiples[1] === 0;
   
-  // Enter the loop and continue as long as 'x' is less than or equal to the second value in the 'range' array
   while (x <= range[1]) {
-    // Check if 'x' is divisible by both 'multiples[0]' and 'multiples[1]'
-    if ((x % multiples[0] === 0) && (x % multiples[1] === 0)) {
-      console.log(words[0] + words[1]); // Print the concatenation of the two words
-    }
-    // Check if 'x' is divisible only by 'multiples[0]'
-    else if (x % multiples[0] === 0) {
-      console.log(words[0]); // Print the first word
-    }
-    // Check if 'x' is divisible only by 'multiples[1]'
-    else if (x % multiples[1] === 0) {
-      console.log(words[1]); // Print the second word
-    }
-    // If none of the above conditions are met, print the value of 'x'
-    else {
-      console.log(x);
-    }
-    
-    // Increment the value of 'x' by 1 to move to the next number in the range
+    let output =
+    (isMultipleOfFirst(x, multiples) && isMultipleOfSecond(x, multiples)) ? `${words[0]}${words[1]}` :
+      (isMultipleOfFirst(x, multiples)) ? words[0] :
+        (isMultipleOfSecond(x, multiples)) ? words[1] :
+          x;
+    console.log(output);
     x++;
   }
-}
-
-
-
-
-
-
+};
 
 
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
